4579914b75d8a894671dae7cf8f1816eec2e1071
Kyle Carson <carsonkk@gmail.com>
2016-03-07 13:06:03 -0800

    Disabled start screen/bg effects, start game from lobby scene and dont use startmenu scene

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,60 +1,56 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
 
 public class CameraManager : MonoBehaviour
 {
+    private WebCamTexture cameraBack;
+    private GameObject cameraBlur;
 
-    public RawImage rawimage;
-    private WebCamTexture webcamTexture;
-
+    //Called when the scene first starts
     public void Start()
     {
-        //var x = new Vector2(GameObject.FindGameObjectWithTag("Canvas").GetComponent<RectTransform>().rect.width, 
-        //    GameObject.FindGameObjectWithTag("Canvas").GetComponent<RectTransform>().rect.height);
-        this.GetComponent<RectTransform>().sizeDelta = new Vector2();
-        webcamTexture = new WebCamTexture();
-        rawimage.texture = webcamTexture;
-        rawimage.material.mainTexture = webcamTexture;
-        webcamTexture.Play();
+        //Set the size of the rect to match the canvas (and thus the user's screen)
+        this.GetComponent<RectTransform>().sizeDelta = new Vector2(
+            GameObject.FindGameObjectWithTag("Canvas").GetComponent<RectTransform>().rect.height,
+            GameObject.FindGameObjectWithTag("Canvas").GetComponent<RectTransform>().rect.width);
+        cameraBlur = GameObject.Find("CameraBlur");
+        cameraBlur.GetComponent<RectTransform>().sizeDelta = new Vector2(
+            GameObject.FindGameObjectWithTag("Canvas").GetComponent<RectTransform>().rect.height,
+            GameObject.FindGameObjectWithTag("Canvas").GetComponent<RectTransform>().rect.width);
+        //Initialize the camera and the raw image's texture
+        cameraBack = new WebCamTexture();
+        this.GetComponent<RawImage>().texture = cameraBack;
+        this.GetComponent<RawImage>().material.mainTexture = cameraBack;
+        cameraBack.Play();
     }
 
     void Update()
     {
-
-        if (webcamTexture.width < 100)
-        {
-            Debug.Log("Still waiting another frame for correct info...");
-            Debug.Log("width is: " + webcamTexture.width.ToString());
-
+        //Wait for the correct camera infoto be obtained from the hardware
+        if (cameraBack.width < 100)
             return;
+
+        //Obtain the rotation of the screen, handling when the image has been mirrored and setting the raw image's rotation based off this
+        var deviceRotation = -cameraBack.videoRotationAngle;
+        if (cameraBack.videoVerticallyMirrored)
+            deviceRotation += 180;
+        this.GetComponent<RawImage>().rectTransform.localEulerAngles = new Vector3(0f, 0f, deviceRotation);
+        cameraBlur.GetComponent<RectTransform>().localEulerAngles = new Vector3(0f, 0f, deviceRotation);
+
+        //Set the aspect ratio to match the new rotation
+        this.GetComponent<RawImage>().GetComponent<AspectRatioFitter>().aspectRatio = cameraBack.width / cameraBack.height;
+        cameraBlur.GetComponent<RawImage>().GetComponent<AspectRatioFitter>().aspectRatio = cameraBack.width / cameraBack.height;
+
+        //Flip the uvRect rendered by the raw image if the image was mirrored
+        if (cameraBack.videoVerticallyMirrored)
+        {
+            this.GetComponent<RawImage>().uvRect = new Rect(1, 0, -1, 1);
+            cameraBlur.GetComponent<RawImage>().uvRect = new Rect(1, 0, -1, 1);
         }
-
-        // change as user rotates iPhone or Android:
-
-        int cwNeeded = webcamTexture.videoRotationAngle;
-        // Unity helpfully returns the _clockwise_ twist needed
-        // guess nobody at Unity noticed their product works in counterclockwise:
-        int ccwNeeded = -cwNeeded;
-
-        // IF the image needs to be mirrored, it seems that it
-        // ALSO needs to be spun. Strange: but true.
-        if (webcamTexture.videoVerticallyMirrored) ccwNeeded += 180;
-
-        // you'll be using a UI RawImage, so simply spin the RectTransform
-        rawimage.rectTransform.localEulerAngles = new Vector3(0f, 0f, ccwNeeded);
-
-        float videoRatio = (float)webcamTexture.width / (float)webcamTexture.height;
-        AspectRatioFitter rawImageARF = rawimage.GetComponent<AspectRatioFitter>();
-        rawImageARF.aspectRatio = videoRatio;
-
-        if (webcamTexture.videoVerticallyMirrored)
-            rawimage.uvRect = new Rect(1, 0, -1, 1);  // means flip on vertical axis
         else
-            rawimage.uvRect = new Rect(0, 0, 1, 1);  // means no flip
-
-        Debug.Log("videoRatio: " + videoRatio.ToString());
-
-
+        {
+            this.GetComponent<RawImage>().uvRect = new Rect(0, 0, 1, 1);
+            cameraBlur.GetComponent<RawImage>().uvRect = new Rect(0, 0, 1, 1);
+        }
     }
 }
--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -7,18 +7,22 @@ public class StartMenu : MonoBehaviour {
     public Button play;
     public Button quit;
 
+    //Called when the scene first starts
 	void Start ()
     {
+        //Assign the play and quit buttons
         play = play.GetComponent<Button>();
         quit = quit.GetComponent<Button>();
     }
 	
+    //If play is pressed, transistion to the lobby scene
     public void PlayPressed()
     {
         SceneManager.LoadScene("lobby");
     }
 
-    public void ExitPressed()
+    //If quit is pressed, quit out of the game
+    public void QuitPressed()
     {
         Application.Quit();
     }
